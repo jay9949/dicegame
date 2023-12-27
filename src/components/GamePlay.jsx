8fe2906557f4e67459fedfbe24f20f178416754d
jay9949 <jay37192@gmail.com>
2023-12-27 10:13:19 +0530

    #

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -23,7 +23,7 @@ const GamePlay = () => {
       return;
     }
 
-    const randomNumber = generateRandomNumber(1, 3);
+    const randomNumber = generateRandomNumber(1, 7);
     setCurrentDice((prev) => randomNumber);
 
     if (selectedNumber === randomNumber) {
